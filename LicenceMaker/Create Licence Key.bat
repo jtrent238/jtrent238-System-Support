8c3212fe7ebe91cfe1d5741c58261edfb36a748d
Trent Patterson <jtrent238@outlook.com>
2018-08-30 20:17:27 -0400

    Add files via upload

--- LicenceMaker/Create Licence Key.bat
+++ LicenceMaker/Create Licence Key.bat
@@ -1,15 +1,19 @@
 @Echo Off
+color 0a
 Setlocal EnableDelayedExpansion
 SET /p user=User:
 Set _RNDLength=25
 Set _Alphanumeric=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 Set _Str=%_Alphanumeric%987654321
+Set Token=null
+
 :_LenLoop
 IF NOT "%_Str:~18%"=="" SET _Str=%_Str:~9%& SET /A _Len+=9& GOTO :_LenLoop
 SET _tmp=%_Str:~9,1%
 SET /A _Len=_Len+_tmp
 Set _count=0
 SET _RndAlphaNum=
+
 :_loop
 Set /a _count+=1
 SET _RND=%Random%
@@ -18,6 +22,14 @@ SET _RndAlphaNum=!_RndAlphaNum!!_Alphanumeric:~%_RND%,1!
 If !_count! lss %_RNDLength% goto _loop
 ::Echo Random string is !_RndAlphaNum!
 cls
+goto createkeyfile
+
+:createkeyfile
+echo { >> %user%.key
+echo user:"!user!", >> %user%.key
+echo key:"!_RndAlphaNum!", >> %user%.key
+echo user_token:"!Token!" >> %user%.key
+echo } >> %user%.key
 goto end
 
 :end
